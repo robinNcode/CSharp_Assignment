a72833c61f8ffae4131b2e4d6bec65362e5e7402
robinNcode <msmrobin518@gmail.com>
2021-12-06 16:37:47 +0600

    properties example done

---
+++ Assignment2/Classes/Properties.cs
@@ -0,0 +1,9 @@
+namespace Assignment2{
+    class Properties{
+        public static string[] words = new string[]{"A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"};
+        //create method to get the value of the property
+        public static int GetValue(int value){
+            return value;
+        }
+    }
+}
---
+++ Assignment2/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using  static Assignment2.Properties;
+namespace Assignment2{
+    public static class Program{
+        public static void Main(){
+            int number = 100;
+            Console.WriteLine(Properties.GetValue(number));
+
+            //Getting all characters from the string
+            string[] charecters = Properties.words;
+            for(int i = 0; i < charecters.Length; i++){
+                Console.WriteLine(charecters[i]);
+            }
+        }
+    }
+}
+
